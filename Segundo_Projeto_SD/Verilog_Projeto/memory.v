25d2d7ca4efaf66955a54341c14be836010aa4dd
Phaidros <oliveiraedson.silva2007@hotmail.com>
2026-06-06 20:05:00 -0300

    Arrumando a memória e a ULA, aplicando clock e transparência através de um REG com os conselhos dados por Pedro (monitor)

--- Segundo_Projeto_SD/Verilog_Projeto/memory.v
+++ Segundo_Projeto_SD/Verilog_Projeto/memory.v
@@ -1,43 +1,41 @@
 module memory (
     // INPUTS que eu vou receber na memória RAM
-    input wire           reset,          // botão ligar
-    input wire           clear,          // INSTRUÇÃO CLEAR (Que age direto na memória, achei melhor deixar um input só para ele)
-    input wire           enable,         // botão enviar
-    input wire [3:0]     write_addr,     // 4 bits  — qual registrador escrever
-    input wire [15:0]    write_data,     // 16 bits — valor a escrever
-    input wire [3:0]     read_addr_1,    // 4 bits  — leitura pra Src1
-    input wire [3:0]     read_addr_2,    // 4 bits  — leitura pra Src2 (instruções reg x reg)
+    input wire           clk,            // Clock da FPGA (50MHZ).
+    input wire           reset,          // Reset.
+    input wire           clear,          // INSTRUÇÃO CLEAR (Que age direto na memória, achei melhor deixar um input só para ele ao invés de fazer a lógica na CPU).
+    input wire [3:0]     write_addr,     // 4 bits   — qual registrador escrever.
+    input wire [15:0]    write_data,     // 16 bits  — valor a escrever.
+    input wire [3:0]     read_addr_1,    // 4 bits   — 1º endereço para um registrador.
+    input wire [3:0]     read_addr_2,    // 4 bits   — 2º endereço para um registrador.
     // OUTPUTS
-    output wire [15:0] read_data_1,
-    output wire [15:0] read_data_2
+    output wire [15:0]   read_data_1,    // O valor de um registrador que a CPU precisar.
+    output wire [15:0]   read_data_2,    // O valor de um segundo registrador que a CPU precisar.
+    output reg           memoryalocated  // Valor para dizer que as memórias foram alocadas, bom para deixar transparente.
 );  
+// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
     // Parametros e declaração para a FSM;
     reg estado_atual, proximo_estado;
     parameter IDLE  = 1'b0, 
               WRITE = 1'b1; // Meus estadinhos
-    reg [15:0] ram [0:15]; // Declaração da RAM
-
-    // Força estado inicial
-    initial begin
-        estado_atual = IDLE;
-    end
-    // Always que controla o fluxo dos estados, note que o enable tá no posedge porque quando ele PRESSIONAR o botão, minha memória já vai pro estado WRITE para quando ele soltar, ele colocar a memória.
-    always @(posedge enable or negedge reset or negedge clear) begin
-        if (!reset || !clear) estado_atual <= IDLE; // reset volta pro IDLE, pronto pra escrever
-        else estado_atual <= proximo_estado;
+    reg [15:0] ram [0:15];  // Declaração da RAM
+// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+    // Always que controla o fluxo dos estados.
+    always @(posedge clk or posedge reset) begin
+        if (reset || clear) estado_atual <= IDLE; // reset ou CLEAR volta pro IDLE, pronto pra escrever
+        else                estado_atual <= proximo_estado;
     end
     // Always que controla a mudança dos estados
     always @(*) begin
         case(estado_atual)
-            IDLE: proximo_estado = WRITE;
-            WRITE: proximo_estado = IDLE;
-            default: proximo_estado = IDLE;
+            IDLE:     proximo_estado = WRITE;
+            WRITE:    proximo_estado = IDLE;
+            default:  proximo_estado = IDLE;
         endcase
     end
     // Always da lógica e inserção de dados na RAM
-    always @(negedge enable or negedge reset or negedge clear) begin
-        // Zerar a memória caso soltar o botão reset ou o comando CLEAR
-        if (!reset || !clear) begin
+    always @(posedge clk or posedge reset) begin
+        // Zerar a memória caso reset ou o comando CLEAR
+        if (reset || clear) begin
             ram[0]  <= 16'b0;
             ram[1]  <= 16'b0;
             ram[2]  <= 16'b0;
@@ -54,15 +52,19 @@ module memory (
             ram[13] <= 16'b0;
             ram[14] <= 16'b0;
             ram[15] <= 16'b0;
+            memoryalocated <= 0;
         end
         else begin
             case (estado_atual)
-                IDLE:;
-                WRITE: ram[write_addr] <= write_data;
-                default:;
+                WRITE: begin
+                    ram[write_addr]      <= write_data;
+                    memoryalocated       <= 1;
+                end  
+                default: memoryalocated  <= 0;
             endcase
         end
     end
+// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
     // Entregar a memória que o sistema pode pedir para a RAM
     assign read_data_1 = ram[read_addr_1];
     assign read_data_2 = ram[read_addr_2];
