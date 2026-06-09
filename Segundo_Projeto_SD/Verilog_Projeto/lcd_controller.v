a44b9d34b50fc5a4cccec364f94d80cd562d6f2b
Renan Adson <renan0409adson@gmail.com>
2026-06-09 14:41:28 -0300

    Update lcd_controller.v

--- Segundo_Projeto_SD/Verilog_Projeto/lcd_controller.v
+++ Segundo_Projeto_SD/Verilog_Projeto/lcd_controller.v
@@ -18,17 +18,21 @@ module lcd_controller (
     reg [19:0] clk_counter;
     reg        tick; 
     reg [3:0]  char_index; // Ponteiro de qual caractere estamos enviando
-    reg [3:0] estado_atual, prox_estado;
+    reg [3:0]  estado_atual, prox_estado;
 
     parameter ST_default     = 4'd0,
-              ST_lcd8b       = 4'd1, // Configura LCD
-              ST_iniciar     = 4'd2, // Liga tela, desliga cursor
-              ST_clr         = 4'd3, // Limpa display
-              ST_avancar     = 4'd4, // Avança cursor
-              ST_escrever_L1 = 4'd5, // Escreve os dados em cima
-              ST_mover_L2    = 4'd6, // Pula para a segunda linha
-              ST_escrever_L2 = 4'd7, // Escreve os dados em baixo
-              ST_feito       = 4'd8;
+              ST_init1       = 4'd1,
+              ST_init2       = 4'd2,
+              ST_init3       = 4'd3,
+              ST_iniciar     = 4'd4,
+              ST_clr         = 4'd5,
+              ST_avancar     = 4'd6,
+              ST_escrever_L1 = 4'd7,
+              ST_mover_L2    = 4'd8,
+              ST_escrever_L2 = 4'd9,
+              ST_feito       = 4'd10,
+              ST_wait_clr1   = 4'd11,
+              ST_wait_clr2   = 4'd12;
 
     // Registrador de Estado
     always @(posedge clk or posedge reset) begin
@@ -41,11 +45,12 @@ module lcd_controller (
     // Controle do contador de clock e do char_index
     always @(posedge clk or posedge reset) begin
         if (reset) begin
+            
             clk_counter <= 20'b0;
             tick        <= 1'b0;
             char_index  <= 4'b0;
         end else begin
-            if (clk_counter == 20'd50_000) begin // Gerador de tick de 1ms
+            if (clk_counter == 20'd49999) begin // Gerador de tick de 1ms
                 clk_counter <= 20'b0;
                 tick        <= 1'b1;
             end else begin
@@ -82,11 +87,15 @@ module lcd_controller (
         LCD_DATA    = 8'h20;  
 
         case (estado_atual)
-            ST_default: prox_estado = start_print ? ST_lcd8b : ST_default;
-            ST_lcd8b:   prox_estado = ST_iniciar;
+            ST_clr:       prox_estado = ST_wait_clr1;
+            ST_wait_clr1: prox_estado = ST_wait_clr2;
+            ST_wait_clr2: prox_estado = ST_avancar;
+            ST_default: prox_estado = start_print ? ST_init1 : ST_default;
+            ST_init1:   prox_estado = ST_init2;
+            ST_init2:   prox_estado = ST_init3;
+            ST_init3:   prox_estado = ST_iniciar;
             ST_iniciar: prox_estado = ST_clr;
-            ST_clr:     prox_estado = ST_avancar;
-            ST_avancar: prox_estado = ST_escrever_L1;
+            ST_avancar:   prox_estado = ST_escrever_L1;
             
             // Espera escrever todos os 16 caracteres da Linha 1
             ST_escrever_L1: if (char_index == 4'd15) prox_estado = ST_mover_L2; 
@@ -101,7 +110,9 @@ module lcd_controller (
 
         // CONTEÚDO DA TELA
         case (estado_atual)
-            ST_lcd8b:    begin LCD_RS = 1'b0; LCD_DATA = 8'h38; end 
+            ST_init1:    begin LCD_RS = 1'b0; LCD_DATA = 8'h38; end
+            ST_init2:    begin LCD_RS = 1'b0; LCD_DATA = 8'h38; end
+            ST_init3:    begin LCD_RS = 1'b0; LCD_DATA = 8'h38; end
             ST_iniciar:  begin LCD_RS = 1'b0; LCD_DATA = 8'h0C; end 
             ST_clr:      begin LCD_RS = 1'b0; LCD_DATA = 8'h01; end 
             ST_avancar:  begin LCD_RS = 1'b0; LCD_DATA = 8'h06; end 
@@ -111,10 +122,10 @@ module lcd_controller (
             ST_escrever_L1: begin
                 LCD_RS = 1'b1;
                 case (char_index)
-                    4'd0: case(opcode) 3'b000:"L"; 3'b001:"A"; 3'b010:"A"; 3'b011:"S"; 3'b100:"S"; 3'b101:"M"; 3'b110:"C"; 3'b111:"D"; default:" "; endcase 
-                    4'd1: case(opcode) 3'b000:"O"; 3'b001:"D"; 3'b010:"D"; 3'b011:"U"; 3'b100:"U"; 3'b101:"U"; 3'b110:"L"; 3'b111:"P"; default:" "; endcase
-                    4'd2: case(opcode) 3'b000:"A"; 3'b001:"D"; 3'b010:"D"; 3'b011:"B"; 3'b100:"B"; 3'b101:"L"; 3'b110:"R"; 3'b111:"L"; default:" "; endcase
-                    4'd3: case(opcode) 3'b000:"D";             3'b010:"I";             3'b100:"I";                                     default:" "; endcase
+                    4'd0: case(opcode) 3'b000:LCD_DATA = "L"; 3'b001:LCD_DATA = "A"; 3'b010:LCD_DATA = "A"; 3'b011:LCD_DATA = "S"; 3'b100:LCD_DATA = "S"; 3'b101:LCD_DATA = "M"; 3'b110:LCD_DATA = "C"; 3'b111:LCD_DATA = "D"; default:LCD_DATA = 8'h20; endcase 
+                    4'd1: case(opcode) 3'b000:LCD_DATA = "O"; 3'b001:LCD_DATA = "D"; 3'b010:LCD_DATA = "D"; 3'b011:LCD_DATA = "U"; 3'b100:LCD_DATA = "U"; 3'b101:LCD_DATA = "U"; 3'b110:LCD_DATA = "L"; 3'b111:LCD_DATA = "P"; default:LCD_DATA = 8'h20; endcase
+                    4'd2: case(opcode) 3'b000:LCD_DATA = "A"; 3'b001:LCD_DATA = "D"; 3'b010:LCD_DATA = "D"; 3'b011:LCD_DATA = "B"; 3'b100:LCD_DATA = "B"; 3'b101:LCD_DATA = "L"; 3'b110:LCD_DATA = "R"; 3'b111:LCD_DATA = "L"; default:LCD_DATA = 8'h20; endcase
+                    4'd3: case(opcode) 3'b000:LCD_DATA = "D";                        3'b010:LCD_DATA = "I";                        3'b100:LCD_DATA = "I";                                                                      default:LCD_DATA = 8'h20; endcase
                     // Mostrador do registrador
                     4'd10: LCD_DATA = "[";
                     4'd11: LCD_DATA = r_bit3;
@@ -143,13 +154,16 @@ module lcd_controller (
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             LCD_EN <= 1'b0;
-        end else begin
-            if (estado_atual != ST_default && estado_atual != ST_feito) begin
-                if (clk_counter > 20'd10 && clk_counter < 20'd30_000)
+        end
+        else begin
+            if (estado_atual != ST_default &&
+                estado_atual != ST_feito) begin
+                if (clk_counter < 20'd50)
                     LCD_EN <= 1'b1;
                 else
                     LCD_EN <= 1'b0;
-            end else begin
+            end
+            else begin
                 LCD_EN <= 1'b0;
             end
         end
